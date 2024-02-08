b3e7316ab0af148ae63574ecd3af6e9d27020f3d
hiu lam tse <hiulam1@hotmail.com>
2024-02-08 11:11:47 +0100

    placed firebase config in eas secrets

--- app.config.js
+++ app.config.js
@@ -1,8 +1,6 @@
 export default {
   expo: {
-    plugins: [
-      "@react-native-google-signin/google-signin",
-    ],
+    plugins: ["@react-native-google-signin/google-signin"],
     name: "test-2",
     slug: "test-2",
     version: "1.0.0",
@@ -35,6 +33,13 @@ export default {
       eas: {
         projectId: "ff662efb-21ad-446a-a028-57d2bcd38b7c",
       },
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
     },
   },
 };
