ff61d9c230c21c330b0049438d64562c88b124bc
hiu lam tse <hiulam1@hotmail.com>
2024-02-17 02:47:58 +0100

    added header to chat screen

--- screens/chatScreen.js
+++ screens/chatScreen.js
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "../components/Header";
 
 const ChatScreen = () => {
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Header title="Chat"></Header>
+    </SafeAreaView>
   );
 };
 
